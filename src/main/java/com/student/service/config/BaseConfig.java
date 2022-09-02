4d3da0dee91d959954b73164e0ad4fd4e7c90d43
Georgiev99 <georgi.georgiev9919@gmail.com>
2022-09-02 20:56:14 +0300

    add different roles for authorization for certain apis

--- src/main/java/com/student/service/config/BaseConfig.java
+++ src/main/java/com/student/service/config/BaseConfig.java
@@ -2,6 +2,8 @@ package com.student.service.config;
 
 
 import com.student.service.entity.Admin;
+import com.student.service.entity.Student;
+import com.student.service.repository.StudentRepository;
 import com.student.service.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +26,9 @@ public class BaseConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         List<Admin> admins = studentService.getAdmins();
@@ -31,6 +36,13 @@ public class BaseConfig extends WebSecurityConfigurerAdapter {
             auth.inMemoryAuthentication()
                     .withUser(admin.getEmail()).password(passwordEncoder().encode(admin
             .getPass()))
+                    .authorities("ROLE_ADMIN");
+        }
+        List<Student> studentList = studentRepository.findAll();
+        for (Student student : studentList){
+            auth.inMemoryAuthentication()
+                    .withUser(student.getFacultyNumber()).password(passwordEncoder().encode(student
+                    .getTIN()))
                     .authorities("ROLE_USER");
         }
     }
@@ -38,8 +50,11 @@ public class BaseConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/securityNone").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/student/info").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/student/all").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/student/create").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/student/delete").access("hasRole('ROLE_ADMIN')")
+    .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
